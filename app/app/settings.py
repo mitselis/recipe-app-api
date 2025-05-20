5ccf893716586a7fbf6816ea981711ecdfa0cf3d
mitselisss <mitselisss@gmail.com>
2025-05-20 12:38:36 +0300

    Added GitHub Actions.

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e3j2mqa2m9l+cig@9@2-t9nrf(+9yr7-p7oc!*h-vp$w1e9@r-'
+#SECRET_KEY = 'django-insecure-e3j2mqa2m9l+cig@9@2-t9nrf(+9yr7-p7oc!*h-vp$w1e9@r-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
